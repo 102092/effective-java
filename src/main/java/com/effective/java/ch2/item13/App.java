ec176abc3d82676de04fbafdc0f37ebfdd0d4223
Han <quaint3@gmail.com>
2020-10-19 22:11:04 +0900

    item12, 13

---
+++ src/main/java/com/effective/java/ch2/item13/App.java
@@ -0,0 +1,47 @@
+package com.effective.java.ch2.item13;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class App implements Cloneable {
+
+  private int a;
+  private int b;
+
+
+  public App(int a, int b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  @Override
+  public App clone() throws CloneNotSupportedException {
+    return (App) super.clone();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    App app = (App) o;
+
+    if (a != app.a) {
+      return false;
+    }
+    return b == app.b;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = a;
+    result = 31 * result + b;
+    return result;
+  }
+}
---
+++ src/main/java/com/effective/java/ch2/item13/Temp.java
@@ -0,0 +1,15 @@
+package com.effective.java.ch2.item13;
+
+public class Temp {
+
+  public static void main(String[] args) throws CloneNotSupportedException {
+    App a1 = new App(1, 2);
+
+    App a2 = (App) a1.clone();
+
+    System.out.println(a1 != a2);
+    System.out.println(a1.getClass() == a2.getClass());
+    System.out.println(a1.equals(a2));
+  }
+
+}
