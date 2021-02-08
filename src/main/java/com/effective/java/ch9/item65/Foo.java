24c972144e525dfe3d68591ac3d5daa087a920a4
102092 <quaint3@gmail.com>
2021-02-08 22:10:28 +0900

    tiem 65 ~ 69

---
+++ src/main/java/com/effective/java/ch9/item65/Foo.java
@@ -0,0 +1,53 @@
+package com.effective.java.ch9.item65;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Set;
+
+/**
+ * @author han
+ */
+
+public class Foo {
+
+    public static void main(String[] args) {
+        Class<? extends Set<String>> cl = null;
+        try {
+            cl = (Class<? extends Set<String>>) Class.forName(args[0]);
+        } catch (ClassNotFoundException e) {
+            fatalError("클래스를 찾을 수 없습니다!");
+        }
+
+        Constructor<? extends Set<String>> cons = null;
+        try {
+            cons = cl.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            fatalError("매개변수 없는 생성자를 찾을 수 없습니다.");
+        }
+
+        Set<String> s = null;
+
+        try {
+            s = cons.newInstance();
+        } catch (InstantiationException e) {
+            fatalError("클래스를 인스턴스화 할 수 없습니다.");
+        } catch (IllegalAccessException e) {
+            fatalError("생성자에 접근할수 없습니다.");
+        } catch (InvocationTargetException e) {
+            fatalError("생성자가 예외를 던졌습니다 "+ e.getCause());
+        } catch (ClassCastException e) {
+            fatalError("Set을 구현하지 않는 클래스 입니다.");
+        }
+
+        s.addAll(Arrays.asList(args).subList(1, args.length));
+        System.out.println(s);
+    }
+
+    private static void fatalError(String msg) {
+        System.out.println(msg);
+        System.exit(1);
+    }
+}
